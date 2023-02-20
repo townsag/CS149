d5510b4104ae6a48ac16318544ecb1b24f9e3bf0
townsag <townsag@gmail.com>
2023-02-20 01:33:34 +0000

    completed search for name inded

--- ASMT1/countnames.c
+++ ASMT1/countnames.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+int name_length = 30;
+int num_names = 100;
+
+int find_name(char names[num_names][name_length], char* name, int num_names){
+        for(int i = 0; i < num_names; i++){
+                if(strcmp(names[i], name) == 0){
+        		return(i);
+		}
+	}
+        return -1;
+}
+
+
 int main(int argc, char* argv[]){
 	if(argc == 1){
 		exit(0);
@@ -12,10 +26,9 @@ int main(int argc, char* argv[]){
 		printf("cannot open file\n");
 	}
 
-	int name_length = 30;
-	int num_names = 100;
 	char names[num_names][name_length];
 	int names_counter[num_names];
+	int num_stored_names = 0;
 
 	//initialize the strings in names to zero and the counter to zero
 	for(int i = 0; i < num_names; i++){
@@ -31,16 +44,24 @@ int main(int argc, char* argv[]){
 	while(fgets(line, name_length, my_file) != NULL){
 		//check to see if an empty line
 		//if so, ignore the line and print to standard error "Warning - line _ is empty.\n"
-		//printf("printing the ascii values of line %d: ", line_counter);
-		//int i = 0;
-		//while(line[i] != '\0'){
-		//	printf("%d, ", line[i]);
-		//	i++;
-		//}
-		//printf("\n");
 		if(strcmp(line, "\n") == 0){
 			fprintf(stderr, "Warning - line %d is empty.\n", line_counter); 
 		} else {
+			//look to see if the name in this line is stored in the names array
+			int location = find_name(names, line, num_names);
+			if(location != -1){
+				printf("found at location %d", location);
+				names_counter[location] = names_counter[location] + 1;
+			}
+			//if so, increment the number of instances of that name in the counter array
+			//else, add the recently read name to the names array and then set the counter
+			//for the recently added name to one and increment the number of stored names
+			else{
+				strcpy(names[num_stored_names], line);
+				//names[num_stored_names] = line;
+				names_counter[num_stored_names] = 1;
+				num_stored_names++;
+			}
 			printf("%s", line);
 		}
 		line_counter++;
