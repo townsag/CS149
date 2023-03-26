dc831474065321a09d398ac6401d223f31e168b5
Andrew Townsend <townsag@gmail.com>
2023-03-26 21:31:48 +0000

    now is successfully reading from stdin and printing to txt and err

--- ASMT3/proc_manager.c
+++ ASMT3/proc_manager.c
@@ -19,10 +19,29 @@ int count_tokens(char* command, char delim){
 	return count;
 }
 
+void write_exit_code(pid_t child_pid, int exit_code){
+	char* out_file_name_str = calloc(20, sizeof(char));                
+	sprintf(out_file_name_str, "%d.txt", child_pid);               
+	int out_file_desc = open(out_file_name_str, O_WRONLY | O_APPEND, 0777);
+	if(out_file_desc == -1){
+		perror("failed to open\n");
+		return;
+	}
+
+	char buffer[40];
+	sprintf(buffer, "Exited with exitcode = %d\n", exit_code); 
+	int len_written = write(out_file_desc, buffer, strlen(buffer));
+	if(len_written == -1){
+		perror("failed to print\n");
+		return;
+	}
+	
+	free(out_file_name_str);
+	close(out_file_desc);
+}
+
 
 int main(int argc, char* argv[]){
-	//char commands[MAX_COMMANDS][LINE_LENGTH];
-	//char** commands = calloc(MAX_COMMANDS * LINE_LENGTH, sizeof(char));
 	char** commands = NULL;
 	int num_commands = 0;
 
@@ -46,40 +65,66 @@ int main(int argc, char* argv[]){
 		if(pid == 0){	//in the child process
 			//change the stdout and stderr files to be pid.out and pid.err
 			pid_t child_pid = getpid();
-			char* file_name_str = calloc(20, sizeof(char));
-		        sprintf(file_name_str, "%d.txt", child_pid);	
-			int out_file_desc = open(file_name_str, O_RDWR | O_CREAT | O_APPEND, 0777);
-			free(file_name_str);
+			pid_t parent_pid = getppid();
+			char* out_file_name_str = calloc(20, sizeof(char));
+			char* err_file_name_str = calloc(20, sizeof(char));
+		        sprintf(out_file_name_str, "%d.txt", child_pid);
+			sprintf(err_file_name_str, "%d.err", child_pid);
+			int out_file_desc = open(out_file_name_str, O_RDWR | O_CREAT | O_APPEND, 0777);
+			int err_file_desc = open(err_file_name_str, O_RDWR | O_CREAT | O_APPEND, 0777);
 			dup2(out_file_desc, STDOUT_FILENO);
+			dup2(err_file_desc, STDERR_FILENO);
+			free(out_file_name_str);
+			free(err_file_name_str);
 			
-			/*
+			printf("Starting command %i: child %d pid of parent %d\n", i, child_pid, parent_pid);
+						
+			fprintf(stderr, "printing iside: %d\n", child_pid);
+						
+			printf("1, should be commands: %s\n",commands[i]);
+			fflush(stdout);	
+
 			//seperate the string in commands[i] into an array of strings
-			int num_tokens = count_tokens(commands[i], ' ');
+			char* temp_command = calloc(strlen(commands[i]) + 1, sizeof(char));
+			strcpy(temp_command, commands[i]);
+
+			int num_tokens = count_tokens(temp_command, ' ');
 			char** args = calloc(num_tokens + 1, sizeof(char*));
 			char* temp;
 			int count = 0;
-			while( (temp = strsep(&commands[i]," ")) != NULL ){
+			while( (temp = strsep(&temp_command," ")) != NULL ){
 				args[count] = temp;
 				count++;
 			}
 			args[count++] = NULL;
-			printf("count: %d, num_tokens: %d\n", count, num_tokens);
+			
+			
+			printf("2, count: %d, num_tokens: %d\n", count, num_tokens);
+			fflush(stdout);
 			//print out all the strings in args
-			for(int i = 0; i < num_tokens + 1; i++){
-				printf("%s\n", args[i]);
-			}*/
+			for(int j = 0; j < num_tokens; j++){
+				printf("%s\n", args[j]);
+				fflush(stdout);
+			}
 
 
+			
 
+			printf("3, last print: %s, %d\n", commands[i], getpid());
+			fflush(stdout);
 
-			printf("%s, %d\n", commands[i], getpid());
+			free(args);
+			free(temp_command);
 			exit(0);
 		} else if (pid < 0){
 			fprintf(stderr,"fork failed\n");
 		}
 	
 	}
-
+	
+	for(int i = 0; i < num_commands; i++){
+		free(commands[i]);
+	}
 
 	return 0;
 }
