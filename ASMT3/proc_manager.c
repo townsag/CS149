af1551276b6fe5395c03101315036de44db37740
Andrew Townsend <townsag@gmail.com>
2023-03-26 00:37:47 +0000

    started assignment 3

---
+++ ASMT3/proc_manager.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+
+#define LINE_LENGTH 30
+#define MAX_COMMANDS 100
+
+void initialize_array(char commands[MAX_COMMANDS][LINE_LENGTH]){
+	//initialize the strings in array to zero
+	for(int i = 0; i < MAX_COMMANDS; i++){
+		for(int j = 0; j < LINE_LENGTH; j++){
+			commands[i][j] = '\0';
+		}
+	}
+}
+
+
+int main(int argc, char* argv[]){
+	char commands[MAX_COMMANDS][LINE_LENGTH];
+	int num_commands = 0;
+	initialize_array(commands);
+
+	char buffer[LINE_LENGTH];
+	while(fgets(buffer, LINE_LENGTH, stdin) != NULL){
+		if(buffer[strlen(buffer) - 1] == '\n'){
+			buffer[strlen(buffer) - 1] = '\0';
+		}
+		strcpy(commands[num_commands], buffer);
+		num_commands++;
+	}
+
+	for(int i = 0; i < num_commands; i++){
+		pid = fork();
+		if(pid == 0){	//in the child process
+			printf("%s, %d\n", commands[i], getpid());
+			exit(0);
+		} else if (pid < 0){
+			fprintf(stde
+	
+	}
+
+
+	return 0;
+}
