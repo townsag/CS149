a00a83dd1a4ed2dc96b2afafab1d5da0ead23a76
Andrew Townsend <townsag@gmail.com>
2023-03-26 17:09:40 +0000

    changed file descripotor

--- ASMT3/proc_manager.c
+++ ASMT3/proc_manager.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define LINE_LENGTH 30
 #define MAX_COMMANDS 100
@@ -34,6 +35,12 @@ int main(int argc, char* argv[]){
 	for(int i = 0; i < num_commands; i++){
 		pid = fork();
 		if(pid == 0){	//in the child process
+			//change the stdout and stderr files to be pid.out and pid.err
+			pid_t child_pid = getpid();
+			char file_name_str[20];
+		        sprintf(file_name_str, "%d.txt", child_pid);	
+			int out_file_desc = open(file_name_str, O_RDWR | O_CREAT | O_APPEND, 0777);
+			dup2(out_file_desc, STDOUT_FILENO);
 			printf("%s, %d\n", commands[i], getpid());
 			exit(0);
 		} else if (pid < 0){
