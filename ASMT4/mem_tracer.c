90ead057ff8f8921f9f4d5a3718745c279b40d2b
Andrew Townsend <townsag@gmail.com>
2023-04-10 22:25:13 +0000

    some bug fixes

--- ASMT4/mem_tracer.c
+++ ASMT4/mem_tracer.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
+//#include <unistd.h>
+//#include <fcntl.h>
+//#include <sys/wait.h>
 
 #define INITIAL_BUFFER_SIZE 10;
 
-char* read_and_allocate(File* input){
+char* read_and_allocate(FILE* input){
+	printf("entering RAA\n");
 	size_t buffer_size = INITIAL_BUFFER_SIZE;
 	char* buffer = (char *)calloc(buffer_size, sizeof(char));
 
@@ -17,14 +18,14 @@ char* read_and_allocate(File* input){
 	}
 
 	char c;
-	size_t index_to_write;
+	size_t index_to_write = 0;
 
-	while((c = fgetc(input)) != '\n && c!= EOF){
+	while((c = fgetc(input)) != '\n' && c!= EOF){
 		//check to see if the buffer is full
-		if(index_to_write == buffer_size -1){	//leave room for null at end of string
+		if(index_to_write == buffer_size - 2){	//leave room for null at end of string
 			//resize the buffer
 			buffer_size *= 2;
-			char temp = (char*) realloc(buffer, buffer_size * sizeof(char));
+			char* temp = (char*) realloc(buffer, buffer_size * sizeof(char));
 			if(temp == NULL){
 				printf("failed to reallocate memory\n");
 				free(buffer);
@@ -38,8 +39,11 @@ char* read_and_allocate(File* input){
 
 	//check for empty buffer and EOF case
 	if(index_to_write == 0 && c == EOF){
+		printf("reached EOF logic\n");
 		return NULL;
 	}
+		
+	printf("leaving RAA\n");
 
 	return buffer;
 }
@@ -57,13 +61,14 @@ int main(int argc, char* argv[]){
 		return(1);
 	}
 
+	printf("starting while loop\n");
 	char* temp_str = NULL;
 	while((temp_str = read_and_allocate(stdin)) != NULL){
 		//check to see if commands is full
 		if(num_commands == commands_size){
 			//reallocate commands
 			commands_size *= 2;
-			temp = (char**) realloc(commands, commands_size * sizeof(char*));
+			char** temp = (char**) realloc(commands, commands_size * sizeof(char*));
 			if(temp == NULL){
 				printf("failed to reallocate commands\n");
 				free(commands);
@@ -74,8 +79,10 @@ int main(int argc, char* argv[]){
 			commands = temp;
 		}
 		commands[num_commands++] = temp_str;
+		printf("you entered: %s\n", commands[num_commands - 1]);
 		temp_str = NULL;
 	}
+	printf("finished while loop\n");
 
 	for(int i = 0; i < num_commands; i++){
 		printf("line %d: %s\n", i, commands[i]);
