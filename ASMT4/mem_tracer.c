cf6be797a65f86127c84bbdaef33b8f06c705eaf
Andrew Townsend <townsag@gmail.com>
2023-04-10 21:30:15 +0000

    added dynamically sized commands array and dynamically sized buffer

---
+++ ASMT4/mem_tracer.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+#define INITIAL_BUFFER_SIZE 10;
+
+char* read_and_allocate(File* input){
+	size_t buffer_size = INITIAL_BUFFER_SIZE;
+	char* buffer = (char *)calloc(buffer_size, sizeof(char));
+
+	if(buffer == NULL){
+		printf("failed to allocate buffer\n");
+		return NULL;
+	}
+
+	char c;
+	size_t index_to_write;
+
+	while((c = fgetc(input)) != '\n && c!= EOF){
+		//check to see if the buffer is full
+		if(index_to_write == buffer_size -1){	//leave room for null at end of string
+			//resize the buffer
+			buffer_size *= 2;
+			char temp = (char*) realloc(buffer, buffer_size * sizeof(char));
+			if(temp == NULL){
+				printf("failed to reallocate memory\n");
+				free(buffer);
+				return NULL;
+			}
+			buffer = temp;
+		}
+		buffer[index_to_write++] = c;
+	}
+	buffer[index_to_write] = '\0';
+
+	//check for empty buffer and EOF case
+	if(index_to_write == 0 && c == EOF){
+		return NULL;
+	}
+
+	return buffer;
+}
+
+
+
+int main(int argc, char* argv[]){
+	char** commands = NULL;
+	int num_commands = 0;
+	int commands_size = INITIAL_BUFFER_SIZE;
+
+	commands = (char**) calloc(commands_size, sizeof(char*));
+	if(commands == NULL){
+		printf("failed to allocate commands\n");
+		return(1);
+	}
+
+	char* temp_str = NULL;
+	while((temp_str = read_and_allocate(stdin)) != NULL){
+		//check to see if commands is full
+		if(num_commands == commands_size){
+			//reallocate commands
+			commands_size *= 2;
+			temp = (char**) realloc(commands, commands_size * sizeof(char*));
+			if(temp == NULL){
+				printf("failed to reallocate commands\n");
+				free(commands);
+				if(temp_str != NULL){
+					free(temp_str);
+				}
+			}
+			commands = temp;
+		}
+		commands[num_commands++] = temp_str;
+		temp_str = NULL;
+	}
+
+	for(int i = 0; i < num_commands; i++){
+		printf("line %d: %s\n", i, commands[i]);
+	}
+
+	for(int i = 0; i < num_commands; i++){
+		free(commands[i]);
+	}
+	free(commands);
+	
+	return 0;
+}
+
+
+
+
+
+
