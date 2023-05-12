fda14c5b9232c7840469c6a22ff7c6a4d5e73d02
Andrew Townsend <townsag@gmail.com>
2023-05-12 20:32:09 +0000

    quicksave before getting lunch

---
+++ ASMT6/test_hash.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "threaded_hash.h"
+
+typedef struct __myarg_t{
+	int n;
+} myarg_t;
+
+typedef struct __myret_t{
+	int n;
+} myret_t;
+
+threaded_hash_table_t* hash_obj;
+
+void* mythread(void* arg){
+	char* names[] = {"aaaa", "bbbb", "cccc", "dddd", "eeee", "ffff", "gggg", "hhhh", "iiii", "jjjj"};
+	myarg_t* m = (myarg_t*) arg;
+	for(int i = 0; i < m->n; i++){
+		add_name(hash_obj, names[i]);
+	}
+	myret_t* r = malloc(sizeof(myret_t));
+	r->n = m->n;
+	return (void*) r;
+}
+
+int main(void){
+	//int rc;
+	hash_obj = new_threaded_hash_table();
+	
+	pthread_t p1;
+	pthread_t p2;
+
+	myret_t* ret1;
+	myret_t* ret2;
+
+	myarg_t args1;
+	myarg_t args2;
+	args1.n = 1000;
+	args2.n = 2000;
+
+	pthread_create(&p1, NULL, mythread, &args1);
+	pthread_create(&p2, NULL, mythread, &args2);
+
+	pthread_join(p1, (void**) &ret1);
+	pthread_join(p2, (void**) &ret2);
+
+	printf("1 returned %d\n", ret1->n);
+	printf("2 returned %d\n", ret2->n);
+
+	print_hash_table(hash_obj);
+	free_hash_table(hash_obj);
+	
+	free(ret1);
+	free(ret2);
+
+	return 0;
+}
--- ASMT6/threaded_hash.c
+++ ASMT6/threaded_hash.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include "threaded_hash.h"
 #include <string.h>
+#include <pthread.h>
 
 node_t* new_node(char* name){
 	node_t* temp = (node_t*)calloc(1, sizeof(node_t));
@@ -22,13 +23,14 @@ void print_node(node_t* node){
 
 
 list_t* new_list(){
-	list_t* temp = (list*)malloc(sizeof(list*));
+	list_t* temp = (list_t*)malloc(sizeof(list_t*));
 	if(temp == NULL){
 		printf("failed to allocate new list\n");
 		return NULL;
 	}
 	temp->head = NULL;
-	pthread_mutex_init(&temp-list_lock, NULL);
+	pthread_mutex_init(&temp->list_lock, NULL);
+	return temp;
 }
 
 void print_list(list_t* list){
@@ -44,8 +46,8 @@ void print_list(list_t* list){
 }
 
 void free_list_recursive(list_t* to_free){
-	node_t temp = to_free->head;
-	node_t look_ahead;
+	node_t* temp = to_free->head;
+	node_t* look_ahead;
 	while(temp != NULL){
 		look_ahead = temp->next;
 		free(temp->name);
@@ -59,7 +61,7 @@ void free_list_recursive(list_t* to_free){
 int insert_list(list_t* list, char* name){
 	node_t* temp = (node_t*)malloc(sizeof(node_t));
 	if(temp == NULL){
-		print("failed to allocate new node for: %s\n", name);
+		printf("failed to allocate new node for: %s\n", name);
 		return -1;
 	}
 	temp->name = name;
@@ -92,7 +94,7 @@ int increment_name(list_t* list, char* name){
 	while(temp != NULL){
 		if(strcmp(temp->name, name) == 0){
 			pthread_mutex_lock(&list->list_lock);
-			temp-count += 1;
+			temp->count += 1;
 			pthread_mutex_unlock(&list->list_lock);
 			return 1;
 		}
@@ -102,13 +104,13 @@ int increment_name(list_t* list, char* name){
 }
 			
 threaded_hash_table_t* new_threaded_hash_table(){
-	threaded_hash_table_t* temp = (threaded_hash_table_t*) calloc(1, sizeof(threaded_hash_table));
+	threaded_hash_table_t* temp = (threaded_hash_table_t*) calloc(1, sizeof(threaded_hash_table_t));
 	if(temp == NULL){
 		printf("failed to allocate hash table struct\n");
 		return NULL;
 	}
 	
-	temp->table = (struct list_t**)calloc(HASH_SIZE, sizeof(struct nlist*));
+	temp->table = (list_t**)calloc(HASH_SIZE, sizeof(struct list_t*));
 	if(temp->table == NULL){
 		printf("failed to allocate hash table table\n");
 		free(temp);
@@ -144,7 +146,7 @@ int lookup_name(threaded_hash_table_t* table_obj, char* name){
 	return search_list(table_obj->table[hash(name)], name);
 }
 
-int add_name(threaded_hash_table* table_obj, char* name){
+int add_name(threaded_hash_table_t* table_obj, char* name){
 	int index = hash(name);
 	int result;
 	if ((result = increment_name(table_obj->table[index], name)) == -1){
